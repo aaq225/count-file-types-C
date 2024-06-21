351a3a819e82e0e55a4af81367ee888ef246109c
Abdelrahman Qamhia <123427105+aaq225@users.noreply.github.com>
2024-06-21 00:07:48 -0700

    Update syscalls.h

--- syscalls.h
+++ syscalls.h
@@ -1,7 +1,7 @@
 #ifndef SYSCALLS_H
 #define SYSCALLS_H
  
-/* put all the prototypes of your wrapped system calls in here */
+
 int Chdir(const char *path);
 DIR *Opendir(const char *name);
 struct dirent *Readdir(DIR *dirp);
